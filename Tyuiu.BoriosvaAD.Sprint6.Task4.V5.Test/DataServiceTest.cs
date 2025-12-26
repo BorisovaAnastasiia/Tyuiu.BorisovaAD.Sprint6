6646986e6821497ffd44ea574af9b26d7d0c59ec
Anastasiia Borisova <kuqhoxp@gmail.com>
2025-12-27 04:52:12 +0500

    Task4

---
+++ Tyuiu.BoriosvaAD.Sprint6.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.BorisovaAD.Sprint6.Task4.V5.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.BorisovaAD.Sprint6.Task4.V5.Test;
+
+[TestClass]
+public sealed class DataServiseTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        DataService ds = new DataService();
+
+        double[] res = ds.GetMassFunction(-5, 5);
+
+        double[] wait =
+        {
+            70.14,
+            55.21,
+            41.05,
+            27.96,
+            15.48,
+            1.0,
+            -13.06,
+            -28.16,
+            -42.96,
+            -56.77,
+            -69.83
+        };
+
+        CollectionAssert.AreEqual(wait, res);
+    }
+}
